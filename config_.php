ef902635b2bb90ff2425b80a5325331072512870
Ali Hamza <miangdpp@gmail.com>
2022-10-18 16:09:14 +0500

    sample configuration

---
+++ config_.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/most_viewed_products/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/most_viewed_products/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/opencart/most_viewed_products/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/opencart/most_viewed_products/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/opencart/most_viewed_products/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mostviewed');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
